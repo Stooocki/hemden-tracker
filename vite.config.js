4a2e6f3c18906c327c08aada35e28fe442cc026b
Stooocki <stocki@gmx.de>
2026-06-21 21:56:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/hemden-tracker/',   // <-- dein GitHub-Repo-Name
+  base: '/hemden-tracker/',
   plugins: [
     react(),
     VitePWA({
@@ -16,14 +16,10 @@ export default defineConfig({
         background_color: '#f5f4f0',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/hemden-tracker/',
-        icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
+        start_url: '/hemden-tracker/'
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,svg}']
       }
     })
   ]
