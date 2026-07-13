62732e05978177b1662c2e089671b5e87ab9c00b
Stooocki <stocki@gmx.de>
2026-07-13 20:58:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['icon-192.png', 'icon-512.png'],
       manifest: {
         name: 'Hemden-Tracker',
         short_name: 'Hemden',
@@ -16,10 +17,14 @@ export default defineConfig({
         background_color: '#f5f4f0',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/hemden-tracker/'
+        start_url: '/hemden-tracker/',
+        icons: [
+          { src: '/hemden-tracker/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/hemden-tracker/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
     })
   ]
